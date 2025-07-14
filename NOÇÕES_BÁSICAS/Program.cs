bfe8cfd932860e194ca075717ac9f2c22fb23938
Klaywin Ryan <klaywindias@gmail.com>
2025-07-14 12:56:04 -0300

    Create Program.cs

---
+++ NOÇÕES_BÁSICAS/Program.cs
@@ -0,0 +1,9 @@
+﻿using Projetos_C_SHARP.Models;
+PessoaFisica PESSOA01 = new PessoaFisica(); //neste caso, o namespace está appontando para a pasta Models, que é onde a classe pessoa está alocada.  
+
+PESSOA01.Nome_da_pessoa = "BUA";
+PESSOA01.Nome_da_empresa = "brisanet";
+PESSOA01.idade = 34;
+PESSOA01.sexo = "masculino";
+
+PESSOA01.Apresentar();
